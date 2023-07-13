86cca65e17fab59812b70585c80d14f89e0c5679
Brian Cerda <cerdabrian26@gmail.com>
2023-07-13 17:28:45 -0500

    Override __str__ in Booking and Menu models.

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -7,8 +7,14 @@ class Booking(models.Model):
     no_of_guests = models.SmallIntegerField(default=0)
     booking_date = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.IntegerField(null=False)
     inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.title
