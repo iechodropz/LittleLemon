b244793d0750c075187f9e130a58bfbba5e3d34f
Brian Cerda <cerdabrian26@gmail.com>
2023-07-14 12:38:06 -0500

    Created psudo code for serializers and generic views for the Menu model.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    pass
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    pass
