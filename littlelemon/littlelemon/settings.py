a73bca66a32142f155fa90e00b98a963c797b579
Brian Cerda <cerdabrian26@gmail.com>
2023-07-17 10:44:46 -0500

    Setting up Authentican using token.

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "restaurant",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DJOSER = {"USER_ID_FIELD": "username"}
